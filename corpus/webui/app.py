692db990539229c5d2f76ac1db72ec6987564caa
Almira <72147905+aRahna@users.noreply.github.com>
2023-12-20 23:36:09 +0300

    add webui

--- corpus/webui/app.py
+++ corpus/webui/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 from ..backend.database import WebDBHandler
 from .. import DB_PATH
@@ -8,15 +8,30 @@ db = WebDBHandler(DB_PATH)
 
 @app.route('/')
 def start_page():
-    return redirect(url_for('search'))
+    return render_template('index.html')
 
 
-@app.route('/search')
-def search():
-    pos = db.get_pos_tags()
-    gloss = db.get_glosses()
-    print(pos, gloss)
-    return
+@app.route('/read', methods=['GET', "POST"])
+def read_page():
+    if request.method == ['GET']:
+        query = request.args
+        print(query)
+        return render_template('read.html')
+    else:
+        return render_template('read.html')
+
+
+@app.route('/search', methods=['GET', "POST"])
+def search_page():
+    if request.method == ['GET']:
+        query = request.args
+        print(query)
+        pos = db.get_pos_tags()
+        gloss = db.get_glosses()
+        print(pos, gloss)
+        return redirect(url_for('results'))
+    else:
+        return render_template('search.html', res=())
 
 
 @app.route('/results', methods=['GET'])
@@ -24,3 +39,4 @@ def results():
     lemma = ''
     pos = ''
     gloss = ''
+    return render_template('search.html', res=([lemma], [pos], [gloss])) #или как там
